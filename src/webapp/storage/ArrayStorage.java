8068b42b8d39f2c1cfe9be30ea88ffc42a0cf07b
Igtaty Kruglov <iggythevegan@gmail.com>
2022-09-08 22:25:04 +0300

    lesson03 HW02

---
+++ src/webapp/storage/ArrayStorage.java
@@ -0,0 +1,74 @@
+package webapp.storage;
+
+import webapp.model.Resume;
+
+import java.util.Arrays;
+
+public class ArrayStorage {
+    private final Resume[] storage = new Resume[10000];
+    private int size = 0;
+
+    public void update(Resume resume) {
+        int index = getIndex(resume.getUuid());
+
+        if (index == -1) {
+            System.out.println("update: no such element: " + resume.getUuid());
+        } else {
+            storage[index] = resume;
+        }
+    }
+
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public void save(Resume r) {
+        if (storage.length <= size) {
+            System.out.println("save: no space in array");
+        } else if (getIndex(r.getUuid()) != -1) {
+            System.out.println("save. resume already exists: " + r.getUuid());
+        } else {
+            storage[size] = r;
+            size++;
+        }
+    }
+
+    public Resume get(String uuid) {
+        int index = getIndex(uuid);
+
+        if (index == -1) {
+            System.out.println("get: no such element: " + uuid);
+            return null;
+        }
+        return storage[index];
+    }
+
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("delete: no such element: " + uuid);
+        } else {
+            size--;
+            storage[index] = storage[size];
+            storage[size] = null;
+        }
+    }
+
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, size);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    protected int getIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
